19ae9fe37118bf35711ac3013eac3cf086b439ac
habeebtheprogrammer <habibamil31@yahoo.com>
2018-06-26 01:00:00 +0100

    new update

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"../", 'build')));
-
+ 
 app.use('/', index);
 app.get('*',function(req, res){
   res.sendFile(path.join(__dirname,"../","build/index.html"))
