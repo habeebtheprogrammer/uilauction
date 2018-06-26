8847db6bb03a3ae38f9b05f96b4402bafa967913
habeebtheprogrammer <habibamil31@yahoo.com>
2018-06-26 01:26:28 +0100

    new update

---
+++ build/js/custom.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $('.addToCart').on("click", function (e) {
+//         $(this).html('')
+//         $(this).append("<i class='fa fa-check'></i>");
+//         $(this).append(' ADDED');
+//         this.disabled = true;
+//         $(this).css({ "background": "#3aba64" })
+//         $(".cart-icon span").css({"background":"#3aba64"})
+//         var discount = $(this).parent().siblings('div');
+//         $(discount).css({ "background": "#3aba64" })
+//     })
+// })
